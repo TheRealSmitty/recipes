247dcd57a9024447fc7efd8bcfeb512337a83b14
TheRealSmitty <chrissmith072390@yahoo.com>
2020-02-26 14:01:42 -0500

    Updates

---
+++ precache-manifest.b6c23e4517485b60551ec4ca77f0790a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82d6c228d7748fb2e89d49e6647e61e9",
+    "url": "/recipes/index.html"
+  },
+  {
+    "revision": "364057e618992150270d",
+    "url": "/recipes/static/css/main.0e3e24be.chunk.css"
+  },
+  {
+    "revision": "5a10a76952a278e96976",
+    "url": "/recipes/static/js/2.2d5a56bd.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/recipes/static/js/2.2d5a56bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "364057e618992150270d",
+    "url": "/recipes/static/js/main.4ec5d0b4.chunk.js"
+  },
+  {
+    "revision": "b523dd487eacbf56ebf2",
+    "url": "/recipes/static/js/runtime-main.3296ca9d.js"
+  }
+]);
